63d43d1f57b34cde25df3d190985a5766014fab0
Pamela <pamela@cedros.dev>
2025-10-06 18:57:35 -0300

    1.0.1 - Production version

--- src/bundler-webview.ts
+++ src/bundler-webview.ts
@@ -57,15 +57,60 @@ export class bundlerProvider implements vscode.WebviewViewProvider {
   }
 
   private async getExternalHTML(): Promise<string> {
-    let script = await fs.promises.readFile(
-      path.resolve(this._extensionUri.fsPath, "src", "bundler-webview.js"),
-      "utf-8"
-    );
     let content = await fs.promises.readFile(
       path.resolve(this._extensionUri.fsPath, "src", "bundler-webview.html"),
       "utf-8"
     );
+    // Get WinnetouJS package version
     const workingFolder = getWinnetouFolderFromWorkspaceSettings();
+    let winnetouVersion = "Unknown";
+    try {
+      const packageJsonPath = workingFolder
+        ? path.join(workingFolder, "node_modules", "winnetoujs", "package.json")
+        : path.join(
+            process.cwd(),
+            "node_modules",
+            "winnetoujs",
+            "package.json"
+          );
+
+      if (fs.existsSync(packageJsonPath)) {
+        const packageJson = JSON.parse(
+          fs.readFileSync(packageJsonPath, "utf-8")
+        );
+        winnetouVersion = packageJson.version;
+      }
+    } catch (error) {
+      console.log("Could not read WinnetouJS version:", error);
+    }
+    content = content.replace("((winnetoujs_version))", winnetouVersion);
+    //-----------------
+    // get latest winnetoujs version from npm
+    let latestVersion: string = await new Promise((resolve, reject) => {
+      exec("npm show winnetoujs version", (error: any, stdout: string) => {
+        if (error) {
+          console.error(`Error fetching latest WinnetouJS version: ${error}`);
+          return resolve("Unknown");
+        }
+        const latestVersion = stdout.trim();
+
+        resolve(latestVersion);
+      });
+    });
+
+    //-----------------
+    // Replace placeholders in the HTML with actual values
+
+    return (
+      content
+        .replace("{codiconUri}", this.codiconUri)
+        .replace("((winnetoujs_folder))", workingFolder || "")
+        //winnetoujs version
+        .replace("((winnetoujs_version))", winnetouVersion)
+        .replace("((latest_winnetoujs_version))", latestVersion)
+        .replace(`// @ts-ignore`, ``)
+        .replace(`//@ts-ignore`, ``)
+    );
     return content;
   }
 }
